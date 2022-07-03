47a15b3ee5a1a9bb9338183ee11baf0762943be7
BinaryBun <chibishew2018@gmail.com>
2022-07-04 01:09:16 +0300

    api erl

---
+++ elibrary.py
@@ -0,0 +1,27 @@
+import sys
+import requests
+from bs4 import BeautifulSoup
+
+
+def main():
+    id = sys.argv[1]
+    code = ""
+    url = f"http://elibrary.ru/projects/API-NEB/API_NEB.aspx?ucode={code}&sid=024&authorid={id}"
+    data = requests.get(url)
+    #print(data, f"{url}&authorid={id}")
+    #print(data.text)
+    soup = BeautifulSoup(data.text, "html.parser")
+
+    autorId = soup.find('authorid').text
+    lastName = soup.find("lastname").text
+    firstName = soup.find("firstname").text
+    secondName = soup.find("secondname").text
+    city = soup.find("city").text
+    orgName = soup.find("orgname").text
+    link = f"http://elibrary.ru/author_profile.asp?id={autorId}"
+
+    with open("jog.txt", 'a', encoding='utf-8') as f:
+        f.write(f"{autorId} | {lastName} | {firstName} | {secondName} | {city} | {orgName} | {link}\n")
+
+if __name__ == '__main__':
+    main()
